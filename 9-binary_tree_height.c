f98a268201d9eb385d011b9fff8f7a31d7cd83a6
pafuentess <pafuentess@unal.edu.co>
2020-03-10 19:48:48 +0000

    task9 check

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,23 @@
 #include "binary_trees.h"
 #include <stdio.h>
+
+/**
+ * binary_tree_node - creates a binary tree node.
+ *
+ *
+ * Return: Always 0.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+       		return 0;
+    	if (node->left == NULL && node->right == NULL)
+        	return 1;
+    	return 0;
+}
+
+
+
 /**
  * binary_tree_node - creates a binary tree node.
  *
@@ -8,15 +26,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t lheight;
-    size_t rheight;
+    int lheight = -1;
+    int rheight = -1;
 
-    if (tree == NULL)
+    if (tree == NULL || binary_tree_is_leaf(tree) == 1) 
         return 0;
     lheight = binary_tree_height(tree->left);
     rheight = binary_tree_height(tree->right);
 
-    if (lheight > rheight)
-        return (lheight + 1);
-    return (rheight + 1);
+    if (lheight >= rheight)
+        return (1 + lheight);
+    return (1 + rheight);
 }
