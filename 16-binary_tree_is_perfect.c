7174795b7fe34cbe4fbcb35d1b7c68aca56b3e22
pafuentess <pafuentess@unal.edu.co>
2020-03-11 20:36:54 +0000

    fix 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -76,6 +76,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	if (binary_tree_is_leaf(tree))
+		return (1);
 	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
 		return (1);
 	return (0);
