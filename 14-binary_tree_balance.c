b27c18e82f2d3687ed3ff5f94fa6a175afba8b97
pafuentess <pafuentess@unal.edu.co>
2020-03-11 15:25:47 +0000

    task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,54 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_node - creates a binary tree node.
+ *
+ *
+ * Return: Always 0.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+       		return 0;
+    	if (node->left == NULL && node->right == NULL)
+        	return 1;
+    	return 0;
+}
+
+
+
+/**
+ * binary_tree_node - creates a binary tree node.
+ *
+ *
+ * Return: Always 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    	int lheight = 0;
+    	int rheight = 0;
+
+    	if (tree == NULL) 
+        	return 0;
+    	lheight = binary_tree_height(tree->left);
+    	rheight = binary_tree_height(tree->right);
+	if (lheight >= rheight)
+        	return (1 + lheight);
+    	return (1 + rheight);
+	
+
+}
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int lheight, rheight;
+	
+	if (tree == NULL)
+		return 0;
+
+	lheight = binary_tree_height(tree->left);
+	rheight = binary_tree_height(tree->right);
+
+	return (lheight - rheight);
+}
