279b69f7500cd4f71f93def8153afbd4c64a6b02
shincap8 <shincap8@gmail.com>
2020-03-11 20:24:04 +0000

    Add more necesary functions

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,40 @@
 #include "binary_trees.h"
 #include <stdio.h>
 /**
+* binary_tree_height - measures the height of a binary tree
+* @tree: node to check
+* Return: height
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int lheight = 0;
+	int rheight = 0;
+
+	if (tree == NULL)
+		return (0);
+	lheight = binary_tree_height(tree->left);
+	rheight = binary_tree_height(tree->right);
+	if (lheight >= rheight)
+		return (1 + lheight);
+	return (1 + rheight);
+}
+/**
+* binary_tree_balance - measures the balance factor of a binary tree
+* @tree: node to check
+* Return: 1 or 0
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int lheight, rheight;
+
+	if (tree == NULL)
+		return (0);
+	lheight = binary_tree_height(tree->left);
+	rheight = binary_tree_height(tree->right);
+
+	return (lheight - rheight);
+}
+/**
 * binary_tree_is_leaf - checks if a node is a leaf
 * @node: parent node
 * Return: 1 or 0
