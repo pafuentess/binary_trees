11a251912ff19a215f447951c99f609b0353fdcc
pafuentess <pafuentess@unal.edu.co>
2020-03-11 19:56:24 +0000

    task 17 y 18 + betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,9 +1,15 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_sibling - finds the sibling of a node
+* @node: is a pointer to the node to find the sibling
+* Return: pointer to the sibling node
+*/
+
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	if (binary_tree_is_full(node->parent) == 1)
 	{
