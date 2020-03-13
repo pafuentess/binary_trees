dd4673328a99ccc818dbbd9bdaa8022ab4c3c7f1
shincap8 <shincap8@gmail.com>
2020-03-13 18:42:55 +0000

    Add if statement

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -14,6 +14,8 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 		tree->left = tree->left->right;
 		tree->parent->right = tree;
 		tree->parent->parent = NULL;
+		if (tree->left)
+			tree->left->parent = tree;
 		return (tree->parent);
 	}
 	return (tree);
